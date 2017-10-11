a4c8b557b4215c52f5698f28a55ad562b0c10de8
AmirolAhmad <amirol@lizard-apps.com>
2017-10-11 15:46:19 +0800

    fixed carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,13 @@ CarrierWave.configure do |config|
 		provider: 'AWS',
 		aws_access_key_id: "#{ENV['S3_ACCESS_KEY_ID']}",
 		aws_secret_access_key: "#{ENV['S3_SECRET_ACCESS_KEY']}",
-		region: 'ap-southeast-1'
+		region: Settings.s3.region
 	}
   config.storage = :fog
 	config.fog_directory = Settings.s3.bucket # required
 	config.fog_public = true # optional, defaults to true
 	config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} # optional, defaults to {}
+	config.asset_host = Settings.image_host
 	config.cache_dir = "#{Rails.root}/tmp/uploads"
 
   if Rails.env.development?
