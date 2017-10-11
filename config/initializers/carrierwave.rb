ec83a327cf7b96ecfec235f78e5ddb166a11e6b0
AmirolAhmad <amirol@lizard-apps.com>
2017-10-11 17:37:16 +0800

    remove image_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,6 @@ CarrierWave.configure do |config|
 	config.fog_directory = Settings.s3.bucket # required
 	config.fog_public = true # optional, defaults to true
 	config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} # optional, defaults to {}
-	config.asset_host = Settings.image_host
 	config.cache_dir = "#{Rails.root}/tmp/uploads"
 
   if Rails.env.development?
