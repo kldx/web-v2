28579e337b9d436efa9669d71e6239c3e3f180de
AmirolAhmad <amirol@lizard-apps.com>
2017-10-12 12:36:18 +0800

    fixed helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def check_status obj
     case obj.status
-    when 1
+    when 0
       "<span class='badge badge-primary'>Normal</span>".html_safe
-    when 2
+    when 1
       "<span class='badge badge-danger'>Busy</span>".html_safe
     else
       "<span class='badge badge-warning'>Unavailable</span>".html_safe
