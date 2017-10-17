3d3b30d855daaafa01d3a1153bbe1de73cf64c01
AmirolAhmad <amirol@lizard-apps.com>
2017-10-18 00:33:22 +0800

    added callback form invoice after_create

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,15 @@
 class Invoice < ApplicationRecord
   belongs_to :order
+
+  after_create :create_invoice_id
+
+  scope :paid, -> { where(state: "paid")   }
+  scope :unpaid, -> { where(state: nil)   }
+  scope :due, -> { where(state: "due")   }
+
+  def create_invoice_id
+    random = [('a'..'z'), ('A'..'Z'), ('1'..'9')].map { |i| i.to_a }.flatten
+    invoice_id = (0...7).map { random[rand(random.length)] }.join
+    self.update_attribute(:invoice_id, invoice_id)
+  end
 end
