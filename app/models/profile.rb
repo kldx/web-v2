d9cdc93e9bea09a427383736780678243b0ae669
AmirolAhmad <amirol@lizard-apps.com>
2017-10-17 18:36:39 +0800

    add method address

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,19 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  def full_address?
+    if no_address
+      "Please update your shipping address"
+    else
+      shipping_address
+    end
+  end
+
+  def shipping_address
+    "#{address}, #{postcode} #{city}, #{state}, #{country}"
+  end
+
+  def no_address
+    address.nil? || postcode.nil? || city.nil? || state.nil? || country.nil?
+  end
 end
