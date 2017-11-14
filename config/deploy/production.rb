2fe78ad2f55f2027c57e50f634052b44932e359c
Amirol Ahmad <dev.amirol@gmail.com>
2017-11-14 18:36:13 +0800

    change from ip to domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,4 @@ set :rails_env, :production
 set :deploy_to, '/home/deploy/kldxteam_production'
 set :release_note_url, 'http://kldxteam.com/release_note.txt'
 
-server '52.221.37.206', user: 'deploy', roles: %w{web app db job}
+server 'kldxteam.com', user: 'deploy', roles: %w{web app db job}
